4f944fb9eee11d6035a720ee6823b125e89deb87
Alexisaccardo <accardoalexis2@gmail.com>
2023-09-11 11:08:55 -0500

    Programa validador de registros de usuarios

---
+++ src/main/java/Usus.java
@@ -0,0 +1,147 @@
+import java.sql.*;
+import java.util.Scanner;
+
+public class Usus {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("*****BIENVENIDOS*****");
+
+        System.out.println("Deseas crear un usuario o eliminar un usuario?: ");
+        String respuesta = scanner.nextLine();
+
+        if (respuesta.equals("crear usuario")) {
+
+            System.out.println("Ingrese el nombre: ");
+            String nombreusuario = scanner.nextLine();
+
+            String nombrebd =   Select_One(nombreusuario);
+            if (nombrebd.equals("")){
+                System.out.println("Ingrese documento de identidad: ");
+                String documento = scanner.nextLine();
+
+                System.out.print("Ingrese su usuario: ");
+                String usuario = scanner.nextLine();
+
+                System.out.print("Ingrese su contrase\u00f1a: ");
+                String pass = scanner.nextLine();
+
+                Insert(nombreusuario, documento, usuario, pass);
+
+                if (nombreusuario.equals("") || documento.equals("") || usuario.equals("") || pass.equals("")) {
+                    System.out.println("No se admiten datos vacios.");
+
+            }
+
+            }else{
+                System.out.println("Este usuario ya se encuentra registrado");
+            }
+
+            }if (respuesta.equals("eliminar usuario")) {
+            System.out.println("Que nombre de usuario deseas eliminar? ");
+            String nombreusuario = scanner.nextLine();
+            String busqueda = Select_One(nombreusuario);
+            if (busqueda.equals("")){
+                System.out.println("Este usuario no se encuentra registrado");
+            }else {
+                Eliminar(nombreusuario);
+            }
+        }
+    }
+
+    private static String Select_One(String Nombre) throws ClassNotFoundException, SQLException {
+        String driver = "com.mysql.cj.jdbc.Driver";
+        String url = "jdbc:mysql://localhost:3306/usuarios";
+        String username = "root";
+        String password = "";
+
+        Class.forName(driver);
+        Connection connection = DriverManager.getConnection(url, username, password);
+
+        String consultaSQL = "SELECT * FROM usus WHERE nombre = ?";
+
+        PreparedStatement statement = connection.prepareStatement(consultaSQL);
+        statement.setString(1, Nombre); // Establecer el valor del parámetro
+
+        // Ejecutar la consulta
+        ResultSet resultSet = statement.executeQuery();
+
+        // Procesar el resultado si existe
+        if (resultSet.next()) {
+            String nombre = resultSet.getString("nombre");
+            String documento = resultSet.getString("documento");
+            String usuario = resultSet.getString("usuario");
+            ;
+
+            return nombre;
+
+        }
+
+        // Cerrar recursos
+        resultSet.close();
+        statement.close();
+        connection.close();
+
+        return "";
+    }
+
+
+    private static void Insert(String nombreusuario, String documento, String usuario, String pass) {
+        String driver = "com.mysql.cj.jdbc.Driver";
+        String url = "jdbc:mysql://localhost:3306/usuarios";
+        String username = "root";
+        String password = "";
+
+        try {
+            Class.forName(driver);
+            Connection connection = DriverManager.getConnection(url, username, password);
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM usus");
+
+
+            // Sentencia INSERT
+            String sql = "INSERT INTO usus (nombre, documento, usuario, pass) VALUES (?, ?, ?, ?)";
+
+            // Preparar la sentencia
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, nombreusuario);
+            preparedStatement.setString(2, documento);
+            preparedStatement.setString(3, usuario);
+            preparedStatement.setString(4, pass);
+
+
+            // Ejecutar la sentencia
+            int filasAfectadas = preparedStatement.executeUpdate();
+
+            if (filasAfectadas > 0) {
+                System.out.println("usuario agregado exitosamente.");
+            } else {
+                System.out.println("No se pudo agregar el usuario.");
+            }
+
+            preparedStatement.close();
+            connection.close();
+            statement.close();
+            resultSet.close();
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void Eliminar(String nombreusuario) throws ClassNotFoundException, SQLException {
+        String driver2 = "com.mysql.cj.jdbc.Driver";
+        String url2 = "jdbc:mysql://localhost:3306/usuarios";
+        String username2 = "root";
+        String pass2 = "";
+
+        Class.forName(driver2);
+        Connection connection2 = DriverManager.getConnection(url2, username2, pass2);
+
+        String sentenciaSQL = "DELETE FROM usus WHERE nombre = ?";
+        PreparedStatement prepareStatement = connection2.prepareStatement(sentenciaSQL);
+        prepareStatement.setString(1, nombreusuario);
+        prepareStatement.executeUpdate();
+
+        System.out.println("Usuario eliminado correctamente");
+    }
+    }
---
+++ usus.sql
@@ -0,0 +1,57 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 11-09-2023 a las 18:05:00
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `usuarios`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usus`
+--
+
+CREATE TABLE `usus` (
+  `nombre` varchar(30) NOT NULL,
+  `documento` varchar(30) NOT NULL,
+  `usuario` varchar(30) NOT NULL,
+  `pass` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usus`
+--
+
+INSERT INTO `usus` (`nombre`, `documento`, `usuario`, `pass`) VALUES
+('johan', '1234567', 'johan10', 'johan102020');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `usus`
+--
+ALTER TABLE `usus`
+  ADD PRIMARY KEY (`nombre`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
